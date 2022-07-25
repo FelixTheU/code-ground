daef056a46b9edb87b43a42b2d3759c6ad70e70e
violet <1162753686@qq.com>
2022-07-26 00:31:44 +0800

    第17题

---
+++ Test/src/topic17.java
@@ -0,0 +1,9 @@
+public class topic17 {
+    public static void main(String[] args) {
+        int x=1;//最后一天的桃子个数
+        for(int i=0;i<10;i++){
+            x=(x+1)*2;
+        }
+        System.out.println("第一天摘得桃子数为"+x);
+    }
+}
